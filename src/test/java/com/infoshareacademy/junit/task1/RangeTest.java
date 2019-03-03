c0aa1295582b9699912a349f5574f22352a88681
Łukasz Waręcki <glenski2be7@gmail.com>
2019-03-03 17:28:23 +0100

    Add @BeforeEach

--- src/test/java/com/infoshareacademy/junit/task1/RangeTest.java
+++ src/test/java/com/infoshareacademy/junit/task1/RangeTest.java
@@ -1,14 +1,33 @@
 package com.infoshareacademy.junit.task1;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RangeTest {
 
+    Range range0;
+    Range range1;
+
+
+    @BeforeAll
+    public static void initALL() {
+
+    }
+
+    @BeforeEach
+    public void initRanges() {
+        //given - przygotowanie danych (sekcje)
+        range0 = new Range(10, 20);
+        range1 = new Range(10, 20);
+
+        System.out.println("BeforeEach");
+    }
+
     @Test
-    public  void shouldReturnTrueIfNumberInRange() {
+    public void shouldReturnTrueIfNumberInRange() {
         //given - przygotowanie danych (sekcje)
         Range range0 = new Range(10, 20);
         Range range1 = new Range(10, 20);
@@ -16,6 +35,12 @@ class RangeTest {
         boolean result = range1.isInRange(12);
         //then
         assertTrue(result, "Number is outside range!");
+    }
+
+    @Test
+    public void shouldReturnTrueIfNumberInRange2() {
+        //then
+        assertNotEquals(range0, range1);
     }
 
     @Test
